2eca2e7d733e9220acd99451fcbdc2fd24c9a8a7
Maksym Davydov <maksym.davydov@booking.com>
2017-04-29 17:25:33 +0200

    Initial state

---
+++ Source/MyFirstProject/MyFirstProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MyFirstProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTPROJECT_API AMyFirstProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
